7db821874922aed9c0009a5ccc3398b73b6d3349
Hugo <hugo.chabert@laplateforme.io>
2021-12-28 18:35:27 +0100

    Création du projet

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+</head>
+<body>
+    <?php require 'header.php' ?>
+<main>
+    <h1>Bienvenue sur le livre d'or de Hugo Chabert</h1>
+</main>
+    <?php require 'footer.php' ?>
+</body>
+</html>
