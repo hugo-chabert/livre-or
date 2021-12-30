f83a429943912ff3448f3ee53461f17dce162a41
Hugo <hugo.chabert@laplateforme.io>
2021-12-30 16:46:54 +0100

    Site fonctionnel

--- index.php
+++ index.php
@@ -6,12 +6,19 @@ require 'fonctions.php';
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="css/index.css" rel="stylesheet">
     <title>Accueil</title>
 </head>
 <body>
     <?php require 'header.php' ?>
 <main>
-    <h1>Bienvenue <?php if(isset($_SESSION['user'])){echo $_SESSION['user']['login'];}?> sur le livre d'or</h1>
+    <div class="container">
+        <h1 class="text_index">Bienvenue <?php if(isset($_SESSION['user'])){echo $_SESSION['user']['login'];}?> sur le livre d'or</h1>
+        <p class="text_index2">Voici mon livre d'or utile pour toutes personnes souhaitant écrire un petit message :)</p>
+        <p class="text_index2">Vous pouvez me commander la création de votre site avec livre d'or sur mon adresse mail hugo.chabert@laplateforme.fr pour seulement 50€ !!!</p>
+        <p class="text_index2">L'apparence du site est customisable selon vos préférences.</p>
+        <p class="text_index2">Cela peut être utile pour un baptême, un mariage ou même pour une personne que vous appréciez.</p>
+    </div>
 </main>
     <?php require 'footer.php' ?>
 </body>
